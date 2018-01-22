8763393138cfdd6326f382ba16da3e36c6c93017
Markus Tacker <Markus.Tacker@NordicSemi.no>
2018-01-22 15:49:47 +0100

    feat(api): initial release

---
+++ README.md
@@ -0,0 +1,11 @@
+# nRF Cloud API ![API v1.0.0-preview.1](https://img.shields.io/badge/API-v1.0.0--preview.1-blue.svg) [![Swagger UI](https://img.shields.io/badge/Swagger-UI-orange.svg)](http://petstore.swagger.io/?url=https%3A%2F%2Fraw.githubusercontent.com%2FnRFCloud%2Fapi%2Fdocs%2Fmaster%2Fapi.json%3Fv1.0.0-preview.1)
+
+[![npm](https://img.shields.io/npm/v/@nrfcloud/api.svg)](https://www.npmjs.com/package/@nrfcloud/api)
+[![Greenkeeper badge](https://badges.greenkeeper.io/nrfcloud/api.svg)](https://greenkeeper.io/)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![Build Status](https://travis-ci.org/nRFCloud/api.svg?branch=master)](https://travis-ci.org/nRFCloud/api)
+[![Greenkeeper badge](https://badges.greenkeeper.io/nRFCloud/api.svg)](https://greenkeeper.io/)
+
+Provides the API documentation and SDK for the [nRFCloud.com](https://nrfcloud.com/) REST API.
---
+++ scripts/extract-schemas-from-api-doc.js
@@ -0,0 +1,49 @@
+/**
+ * This file builds the interfaces from the JSON schema in the swagger definition.
+ */
+
+const {readFile: readFileAsync, writeFile: writeFileAsync} = require('fs')
+const {promisify} = require('util')
+const readFile = promisify(readFileAsync)
+const writeFile = promisify(writeFileAsync)
+const path = require('path')
+
+const srcFile = process.argv[process.argv.length - 2]
+const targetDir = process.argv[process.argv.length - 1]
+
+readFile(srcFile, 'utf-8')
+  .then(JSON.parse)
+  .then(api => Promise
+    .all(
+      Object.keys(api.components.schemas).map(schemaName => {
+        const schema = api.components.schemas[schemaName]
+        const {title} = schema
+        return writeFile(path.join(targetDir, `${title}.json`), JSON.stringify(fixSchemaReferences(schema), null, 2))
+      }))
+    .then(() => {
+      // Write index files
+      const indexJs = '"use strict";\n' +
+        'Object.defineProperty(exports, "__esModule", { value: true });\n' +
+        'exports.schemas = [\n' +
+        Object.keys(api.components.schemas).map(schemaName => `require('./schemas/${schemaName}.json')`).join(',\n') +
+        '\n];'
+      return Promise.all([
+        writeFile('./dist/index.js', indexJs),
+        writeFile('./dist/index.d.ts', Object.keys(api.components.schemas).map(schemaName => `export * from './types/${schemaName}.d.ts';`).join('\n'))
+      ])
+    })
+  )
+
+const fixSchemaReferences = (object, key) => {
+  if (object === Object(object)) {
+    const keys = Object.keys(object)
+    return keys.reduce((object, key) => {
+      object[key] = fixSchemaReferences(object[key], key)
+      return object
+    }, object)
+  }
+  if (key === '$ref') {
+    return object.replace('#/components/schemas/', './').replace(/$/, '.json')
+  }
+  return object
+}
